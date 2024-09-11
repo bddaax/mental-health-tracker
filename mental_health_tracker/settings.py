14755cc78611cc295ea1925df350c5e10663e40c
bddaax <brenda147052gmail.com>
2024-09-11 16:37:13 +0700

    trigger gh

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -120,5 +120,6 @@ STATIC_URL = 'static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://brenda-po-mentalhealthtracker2.pbp.cs.ui.ac.id", "http://brenda-po-mentalhealthtracker2.pbp.cs.ui.ac.id"]
